62c90edfea78f0446e7c83638ed2b6d31cf61ab0
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2022-10-18 00:24:45 +0700

    add: clean unuse file.

---
+++ pages/api/wordle/index.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import wordleWord from "../../../data/wordle-word";
+import { WordleQuery } from "../../../interfaces";
+
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+	//NOTE - API GET method perform fumction
+	const apiGET = () => {
+		res.status(200).json(filerQuery(_req.query));
+	};
+
+	//NOTE - Get the query from API query and filter word that include letter in query and filter word that has perfect match placement
+	const filerQuery = (query: WordleQuery) => {
+		let result = wordleWord;
+
+		//NOTE - filer word that include letter
+		if (query.filter) {
+			let buffer: string[];
+			const filter = query.filter.split("");
+			for (const letterPos in filter) {
+				buffer = result.filter((word) => !word.includes(filter[letterPos]));
+			}
+			result = buffer;
+		}
+
+		//NOTE - filter word that has match placement
+		if (query.perfect) {
+			const perfect = query.perfect.split("");
+			let buffer = result;
+
+			for (const letterPos in perfect) {
+				if (perfect[letterPos] !== "0") {
+					buffer = buffer.filter(
+						(word) => word[letterPos] === perfect[letterPos]
+					);
+				}
+			}
+			result = buffer;
+		}
+		return result;
+	};
+
+	try {
+		switch (_req.method) {
+			case "GET":
+				apiGET();
+				break;
+		}
+	} catch (err: any) {
+		res.status(500).json({ statusCode: 500, message: err.message });
+	}
+};
+
+export default handler;
