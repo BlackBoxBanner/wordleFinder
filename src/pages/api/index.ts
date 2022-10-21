5040ab5785e977e38ae6950cf58676bc38d7efe2
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2022-10-21 23:45:50 +0700

    fix: api type error

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,8 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+interface Data {
+  gread:string
+}
+interface S500 {
+  statusCode: number
+  message: string
+}
+
+const handler = (_req: NextApiRequest, res: NextApiResponse<Data | S500>) => {
   try {
-    res.status(200).json("Index API")
+    res.status(200).json({gread:"test"})
   } catch (err: any) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
